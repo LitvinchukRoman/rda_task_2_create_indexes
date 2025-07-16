5ed4fb903088cd602a4274efe24d263e0dc7843d
litvi <litvin4uk.roman@gmail.com>
2025-07-16 18:19:37 +0300

    Solution

--- task.sql
+++ task.sql
@@ -2,4 +2,8 @@ USE ShopDB;
 CREATE INDEX Name ON Products (Name);
 CREATE INDEX FirstName ON Customers (FirstName);
 CREATE INDEX LastName ON Customers (LastName);
+CREATE INDEX Email ON Customers (Email);
+CREATE INDEX Address ON Customers (Address);
+
+
 
